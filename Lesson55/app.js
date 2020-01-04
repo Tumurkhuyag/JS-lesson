5274046bab2b76792e4f488c1891e052fe58995c
Tumurkhuyag <tomor.enkh@gmail.com>
2020-01-04 18:34:59 +0800

    55 done

--- Lesson55/app.js
+++ Lesson55/app.js
@@ -1,11 +1,51 @@
-function Materi(color) {
+function Materi(color, size) {
   this.color = color;
+  this.size = size;
   this.name = "биет";
 }
 
 Materi.prototype.seen = function() {
-  console.log(this.color + " өнгөтэй " + this.name + " харагдлаа");
+  console.log(
+    this.color + " өнгөтэй " + this.size + " " + this.name + " харагдлаа"
+  );
 };
 
-var m1 = new Materi("ягаан");
+var m1 = new Materi("ягаан", "том");
 m1.seen();
+
+function Animal(name, color, size) {
+  Materi.call(this, color, size);
+  this.name = name;
+}
+
+Animal.prototype = Object.create(Materi.prototype);
+
+var cat = new Animal("муур", "эрээн", "бяцхан");
+console.log(cat.name);
+console.log(cat.color);
+cat.seen();
+
+function Person(name, job, color, size) {
+  Animal.call(this, job, color, size);
+  this.name = name;
+  this.job = job;
+}
+
+Person.prototype = Object.create(Animal.prototype);
+Person.prototype.speak = function(word) {
+  console.log(
+    this.name +
+      " нэртэй " +
+      this.job +
+      " мэргэжилтэй " +
+      this.color +
+      " арьстай " +
+      this.size +
+      ' нуруутай хүн "' +
+      word +
+      '" гэж хэллээ...'
+  );
+};
+
+var p1 = new Person("Батаа", "хөгжүүлэгч", "шар", "өндөр");
+p1.speak("JS лайтай юмаа");
