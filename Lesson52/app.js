50a734dbd2894882b29e6405e8435f800483414c
Tumurkhuyag <tomor.enkh@gmail.com>
2020-01-04 13:25:32 +0800

    Lesson52 done

--- Lesson52/app.js
+++ Lesson52/app.js
@@ -56,3 +56,78 @@ console.log(
 console.log("p2.clothes.__proto__ бол " + typeof p2.clothes.__proto__);
 
 console.log(p2.clothes.constructor.name);
+
+// String function -ийг өргөтгөе
+String.prototype.sainuu = function() {
+  console.log("Good afternoon!!!!!");
+  return "sainuu function -ийн утга";
+};
+
+console.log("Энэ бол string".sainuu());
+
+console.log(typeof String.prototype.sainuu);
+console.log(String.prototype.sainuu.__proto__);
+console.log(String.prototype.sainuu.prototype);
+console.log(String.prototype);
+console.log(String);
+console.log(
+  "String.prototype нь " +
+    String.prototype +
+    " бөгөөд type нь " +
+    typeof String.prototype +
+    " юм"
+);
+console.log("String нь " + String + " бөгөөд type нь " + typeof String + " юм");
+
+// Объектэйс объект удамшуулах замаар materi -> animal -> person -> developer гэсэн 4 объект үүсгэе
+null;
+var Object;
+var materi = { name: "биет" };
+materi.seen = function() {
+  console.log(this.name + " харагдлаа");
+};
+
+var animal = Object.create(materi);
+animal.name = "амьтан";
+animal.eat = function() {
+  console.log(this.name + " хоололлоо");
+};
+animal.move = function() {
+  console.log(this.name + " хөдөллөө");
+};
+
+var person = Object.create(animal);
+person.name = "хүн";
+person.getOrder = function(order) {
+  console.log(this.name + " " + order + " гэсэн тушаал авлаа");
+};
+
+var developer = Object.create(person);
+developer.name = "Болдоо";
+developer.job = "хөгжүүлэгч";
+developer.skill = "апп -ын кодыг бичсэн";
+developer.developApp = function(appName) {
+  console.log(this.name + " " + this.job + " " + appName + " " + this.skill);
+};
+
+var designer = Object.create(person);
+designer.name = "Төмөрөө";
+designer.job = "дизайнер";
+designer.skill = "апп -ын дизайныг гаргасан";
+designer.designApp = function(appName) {
+  console.log(this.name + " " + this.job + " " + appName + " " + this.skill);
+};
+
+console.log(developer.name);
+console.log(developer);
+console.log(developer.__proto__);
+console.log(developer.__proto__.__proto__);
+console.log(developer.__proto__.__proto__.__proto__);
+console.log(developer.__proto__.__proto__.__proto__.__proto__);
+console.log(developer.__proto__.__proto__.__proto__.__proto__.__proto__);
+
+developer.seen();
+animal.seen();
+
+designer.designApp("MemoWords");
+developer.developApp("MemoWords");
