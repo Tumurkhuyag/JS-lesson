c86d5e4f683a8b24a29c88638f83627294127d94
Tumurkhuyag <tomor.enkh@gmail.com>
2020-01-06 21:26:40 +0800

    lesson56 done

---
+++ Lesson56/app.js
@@ -0,0 +1,44 @@
+// var math = {
+//   abs: function(number) {
+//     if (number < 0) return 0 - number;
+//     else return number;
+//   }
+// };
+
+// console.log(typeof Math);
+// console.log(Math.abs(-20));
+
+// console.log(math.abs(-300));
+
+// console.log(Math.floor(12.4));
+// console.log(Math.ceil(12.4));
+// console.log(Math.round(12.4));
+
+// console.log(Math.sqrt(16));
+
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+var birthYears = [1981, 2005, 1990, 1988, 1985];
+
+function findOutAge() {
+  var ages = [];
+  for (var i = 0; i < birthYears.length; i++) {
+    age = new Date().getFullYear() - birthYears[i];
+    ages.push(age);
+  }
+  return ages;
+}
+
+var ages = findOutAge();
+console.log(ages);
+
+function isAdultPerson() {
+  var isAdult = [];
+  for (var i = 0; i < ages.length; i++) {
+    isAdult.push(18 <= ages[i]);
+  }
+  return isAdult;
+}
+
+var isAdult = isAdultPerson();
+console.log(isAdult);
