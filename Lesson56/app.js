035e6802ef5a5d5fbc9a4dc1f4b33d46ff3732ac
Tumurkhuyag <tomor.enkh@gmail.com>
2020-01-08 06:18:53 +0800

    Update app.js

--- Lesson56/app.js
+++ Lesson56/app.js
@@ -56,19 +56,95 @@
 
 // --------------------------------------------------------------------------------
 
+// var birthYears = [1951, 2005, 1990, 1988, 1985, 1955];
+// var bodyHeight = [170, 150, 180, 167, 157, 153];
+// var bodyWeight = [80, 45, 75, 62, 63, 65];
+// var isMale = [true, false, true, true, false, false];
+// var ages = processArray(birthYears, findOutAges);
+
+// // console.log(processArray(ages, checkAdult));
+// var bmr = processArray(isMale, findBMR);
+
+// // Array болон функцийн утгыг дамжуулж шинэ array үүсгэх үндсэн функц
+// function processArray(myArr, fn) {
+//   var arr = [];
+//   for (i = 0; i < myArr.length; i++) {
+//     arr.push(fn(myArr[i], i));
+//   }
+//   return arr;
+// }
+
+// // Нас олох функц
+// function findOutAges(el) {
+//   return new Date().getFullYear() - el;
+// }
+
+// // Насанд хүрсэн эсэхийг шалгах функц
+// function checkAdult(el) {
+//   return 18 <= el;
+// }
+
+// // Тэтгэвэрт гарсан эсэхийг шалгах anonymous функц
+// var isRetired = processArray(ages, function(el) {
+//   return 65 <= el;
+// });
+// // console.log(isRetired);
+
+// // Basal Metabolic Rate (BMR) олох функц
+// // Women: BMR = 655 + (9.6 * weight in kg) + (1.8 * height in cm) - (4.7 * age in years)
+// // Men: BMR = 66 + (13.7 * weight in kg) + (5 * height in cm) - (6.8 * age in years)
+// function findBMR(el, i) {
+//   if (el)
+//     return Math.round(
+//       66 + 13.7 * bodyWeight[i] + 5 * bodyHeight[i] - 6.8 * ages[i]
+//     );
+//   else
+//     return Math.round(
+//       655 + 9.6 * bodyWeight[i] + 1.8 * bodyHeight[i] - 4.7 * ages[i]
+//     );
+// }
+
+// function calorieAdvice() {
+//   for (i = 0; i < bmr.length; i++) {
+//     if (isMale[i]) person = "эрэгтэй";
+//     else person = "эмэгтэй";
+
+//     console.log(
+//       birthYears[i] +
+//         " онд төрсөн " +
+//         ages[i] +
+//         " настай " +
+//         bodyHeight[i] +
+//         "см өндөртэй " +
+//         bodyWeight[i] +
+//         "кг жинтэй " +
+//         person +
+//         " өдөрт ойролцоогоор " +
+//         bmr[i] +
+//         " калориос ихгүй хоол хүнс хэрэглэвэл зохино"
+//     );
+//   }
+// }
+
+// calorieAdvice();
+
+// --------------------------------------------------------------------------------
+
 var birthYears = [1951, 2005, 1990, 1988, 1985, 1955];
 var bodyHeight = [170, 150, 180, 167, 157, 153];
 var bodyWeight = [80, 45, 75, 62, 63, 65];
 var isMale = [true, false, true, true, false, false];
 var ages = processArray(birthYears, findOutAges);
 
-// console.log(processArray(ages, checkAdult));
+// Тухайн хүсүүсийн хүйс, болон BMR олох функцыг processArray функцруу дамжуулж гарсан утгыг нь bmr array -д хадгалах
 var bmr = processArray(isMale, findBMR);
 
 // Array болон функцийн утгыг дамжуулж шинэ array үүсгэх үндсэн функц
 function processArray(myArr, fn) {
   var arr = [];
   for (i = 0; i < myArr.length; i++) {
+    // "isMale" массив "myArr" -ын утгад дамжуулагдсан, "findBMR" функц "fn" функцын утгад дамжуулагдсан
+    // "myArra[i]" дугаар элемент, болон "i" буюу i дугаар элементийг заагч тоог fn функцруу дамжуулан бололвсруулж, гарсан утгыг нь хүлээж аван "arr" массивт урдаас нь хойш нь залгах кодчилол
     arr.push(fn(myArr[i], i));
   }
   return arr;
@@ -79,31 +155,23 @@ function findOutAges(el) {
   return new Date().getFullYear() - el;
 }
 
-// Насанд хүрсэн эсэхийг шалгах функц
-function checkAdult(el) {
-  return 18 <= el;
-}
-
-// Тэтгэвэрт гарсан эсэхийг шалгах anonymous функц
-var isRetired = processArray(ages, function(el) {
-  return 65 <= el;
-});
-// console.log(isRetired);
-
 // Basal Metabolic Rate (BMR) олох функц
 // Women: BMR = 655 + (9.6 * weight in kg) + (1.8 * height in cm) - (4.7 * age in years)
 // Men: BMR = 66 + (13.7 * weight in kg) + (5 * height in cm) - (6.8 * age in years)
 function findBMR(el, i) {
+  // Эрэгтэй бол ажиллах томъёо
   if (el)
     return Math.round(
       66 + 13.7 * bodyWeight[i] + 5 * bodyHeight[i] - 6.8 * ages[i]
     );
+  // Эмэгтэй бол ажиллах томъёо
   else
     return Math.round(
       655 + 9.6 * bodyWeight[i] + 1.8 * bodyHeight[i] - 4.7 * ages[i]
     );
 }
 
+// Console дотор мэдээлэл хэвлэх төдий функц
 function calorieAdvice() {
   for (i = 0; i < bmr.length; i++) {
     if (isMale[i]) person = "эрэгтэй";
