fec85433e7a146b8852773426ec9353339f17feb
Tumurkhuyag <tomor.enkh@gmail.com>
2020-01-24 17:15:13 +0800

    Lesson62 done

--- Lesson62/app.js
+++ Lesson62/app.js
@@ -1,1 +1,44 @@
-Hi;
+var uiController = (function() {
+  //   console.log("This is uiController anonymous function");
+  var x = 140;
+
+  function add(y) {
+    return x + y;
+  }
+
+  return {
+    publicAdd: function(a) {
+      a = add(a);
+      console.log("боловсруулсан утга : " + a);
+    }
+  };
+})();
+
+var financeController = (function() {
+  //   console.log("That is financeController anonymous function");
+})();
+
+var appController = (function(uiController, financeController) {
+  uiController.publicAdd(150);
+  //   console.log("This is appController anonymous function");
+})(uiController, financeController);
+
+// var hunController = (function() {
+//   // data encapsulation
+//   // private data
+//   var bodol = "Javascript толгой эргүүлмээр юм...";
+//   // private function
+//   function tsusGuih() {}
+//   function huchilturugchiigAgaaraasSorjTsusruuOruulah() {}
+
+//   return {
+//     yarih: function() {
+//       bodol = "Javascript бол лаг";
+//       huchilturugchiigAgaaraasSorjTsusruuOruulah();
+//       tsusGuih();
+//       console.log("hi " + bodol);
+//     }
+//   };
+// })();
+
+// hunController.yarih();
