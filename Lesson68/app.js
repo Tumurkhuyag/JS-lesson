2d506e78e5fe060387e0fdc3610e3def3a4c6ebc
Tumurkhuyag <tomor.enkh@gmail.com>
2020-02-07 02:32:23 +0800

    Lesson68

---
+++ Lesson68/app.js
@@ -0,0 +1,8 @@
+// var html = "Сайн уу";
+// html = html.replace("уу", "байна уу");
+// console.log(html);
+
+var incDiv = document.querySelector(".income");
+incDiv.insertAdjacentHTML("beforeend", "Цалин : 5,000,000₮");
+incDiv.insertAdjacentHTML("beforeend", "<br>Төсөл : 3,000,000₮");
+incDiv.insertAdjacentHTML("beforeend", "<br>Төсөл : 500,000₮");
