45122eb70e013a2cda3c4ae6e44c1383cbc712c2
Tumurkhuyag <tomor.enkh@gmail.com>
2020-02-08 22:09:33 +0800

    Lesson69 done

---
+++ Lesson69/app.js
@@ -0,0 +1,13 @@
+// https://playcode.io/ дээр туршиж үзээрэй
+
+var arr = ["orange", "apple", "banana", "grape", "berry"];
+
+// var arr = new Array();
+// arr.
+
+console.log(typeof arr);
+console.log(arr.__proto__.constructor);
+
+newArr = arr.slice(1, 3);
+
+console.log(newArr);
